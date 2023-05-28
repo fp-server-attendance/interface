5cb74567cbfc1707fb721b140824c02a4a1aea03
ilyas <ilyascoskun200@gmail.com>
2023-05-29 01:24:49 +0300

    added mainPage tests

--- test/test.js
+++ test/test.js
@@ -7,13 +7,17 @@ var assert = chai.assert;
 // using nock library to stimulate server responses
 const nock = require('nock');
 // functions in login.js
-const { sendDataForTesting } = require('../login');
-const { sendData } = require('../login');
-const { getData } = require('../mainPage');
+const { sendDataForTesting } = require('../testLogin');
+const { sendData } = require('../testLogin');
+// functions in mainPage.js
+const { getData } = require('../testMainPage');
+const { getDataForTesting } = require('../testMainPage');
 
+
+const sessionIdTest = "";
 describe('Login Tests', () => {
   it('server should connect successfully', function (done) {
-    chai.request('http://54.174.149.55:8080')
+    chai.request('http://3.226.237.9:8080')
       .get('/')
       .end(function (err, res) {
         expect(err).to.be.null;
@@ -26,13 +30,12 @@ describe('Login Tests', () => {
     const username = 'seraslan';
     const password = 'abc123';
     // using nock library to stimulate server responses
-    nock('http://54.174.149.55:8080')
+    nock('http://3.226.237.9:8080')
       .post('/teacher/authenticate', { username, password })
       .reply(404, { success: true });
-
+    
     // Exercise
     const result = await sendDataForTesting(username, password);
-
     // Verify
     expect(result).to.be.true;
   });
@@ -42,13 +45,13 @@ describe('Login Tests', () => {
     const username = 'wrongUsername';
     const password = 'wrongPassword';
 
-    nock('http://54.174.149.55:8080')
+    nock('http://3.226.237.9:8080')
       .post('/teacher/authenticate', { username, password })
       .reply(404, { success: false });
 
     // Exercise
     const result = await sendDataForTesting(username, password);
-
+    
     // Verify
     expect(result).to.be.false;
   });
@@ -58,7 +61,7 @@ describe('Login Tests', () => {
     const username = 'wrongUsername';
     const password = 'wrongPassword';
 
-    nock('http://54.174.149.55:8080')
+    nock('http://3.226.237.9:8080')
       .post('/teacher/authenticate', { username, password })
       .reply(404, { success: false });
 
@@ -73,49 +76,45 @@ describe('Login Tests', () => {
 });
 describe('Main Page Tests', () => {
   it('server should connect successfully', function (done) {
-    chai.request('http://54.174.149.55:8080')
+    chai.request('http://3.226.237.9:8080')
       .get('/')
       .end(function (err, res) {
         expect(err).to.be.null;
         expect(res).to.have.status(404);
         done();
-      });
+      }); 
   });
   it('should list courses successfully with valid credentials', async function() {
-    const data = {
-      sectionYear: '2023',
-      semester: 'fall',
-      teacherUserName: 'seraslan',
-      sessionId: "c07f3e4f-9efe-4de2-abc0-40ecd84f4b74"
-    };
+    const sectionYear = '2023';
+    const semester = 'SPRING';
+    const teacherUserName = 'seraslan';
+    const sessionId = "4ed0f6ca-31ef-41c3-9e57-fac3b9a8297e";
 
     // using nock library to stimulate server responses
-    nock('http://54.174.149.55:8080')
-      .post('/teacher/courses', data)
+    nock('http://3.226.237.9:8080')
+      .post('/teacher/courses', {sectionYear, semester, teacherUserName, sessionId})
       .reply(404, { success: true });
 
     // Exercise
-    const result = await getData();
-
+    const result = await getDataForTesting();
+    
     // Verify
     expect(result).to.be.true;
   });
 
   it('should fail to list courses with invalid credentials', async function() {
-    const data = {
-      sectionYear: '2023',
-      semester: 'fall',
-      teacherUserName: 'wrongUserName',
-      sessionId: "c07f3e4f-9efe-4de2-abc0-40ecd84f4b74"
-    };
+    const sectionYear = '2023';
+    const semester = 'fall';
+    const teacherUserName = 'seraslan';
+    const sessionId = "c07f3e4f-9efe-4de2-abc0-40ecd84f4b74";
 
     // using nock library to stimulate server responses
-    nock('http://54.174.149.55:8080')
-      .post('/teacher/courses', data)
+    nock('http://3.226.237.9:8080')
+      .post('/teacher/courses', {sectionYear, semester, teacherUserName, sessionId})
       .reply(404, { success: false });
 
     // Exercise
-    const result = await getData();
+    const result = await getDataForTesting();
 
     // Verify
     expect(result).to.be.false;
