ddd5eab57f23ae00db99204bff63bde1d065a5b2
ilyas <ilyascoskun200@gmail.com>
2023-06-04 02:40:20 +0300

    test js file for register

---
+++ Test Js Files/testRegister.js
@@ -0,0 +1,87 @@
+const getBtn = document.getElementById('get-lecture-btn');
+const postBtn = document.getElementById('send-login-btn');
+
+const sendHttpRequest = (method, url, data) => {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: data ? { 'Content-Type': 'application/json' } : {}
+  }).then(response => {
+    if (response.status >= 400) {
+      return response.json().then(errResData => {
+        const error = new Error('Something went wrong!');
+        error.data = errResData;
+        throw error;
+      });
+    }
+    return response.json();
+  });
+};
+
+const sendData = (username, password, cpassword, fName, lName, email) => {
+  sendHttpRequest('POST', 'http://54.227.120.179:8080/teacher/add', {
+    username: username,
+    password: password,
+    cpassword: cpassword,
+    fName: fName,
+    lName: lName,
+    email: email
+
+  })
+    .then(responseData => {
+      if (responseData.success) {
+        console.log(responseData.sessionId);
+        alert('You are signed up.');
+        // directing to login page with user credentials if register is succesfull
+        window.location.href = './login.html';
+      } else {
+        alert('Please check your login information.');
+      }
+    })
+    .catch(err => {
+      console.log(err, err.data);
+    });
+};
+
+const sendDataForTesting = (username, password, cpassword, fName, lName, email) => {
+    sendHttpRequest('POST', 'http://54.227.120.179:8080/teacher/add', {
+      username: username,
+      password: password,
+      cpassword: cpassword,
+      fName: fName,
+      lName: lName,
+      email: email
+  
+    })
+      .then(responseData => {
+        if (responseData.success) {
+            return responseData.success;
+        } else {
+            return false;
+        }
+      })
+      .catch(err => {
+        console.log(err, err.data);
+      });
+  };
+
+// get data from html page
+postBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const usernameInput = document.querySelector('input[name="username"]');
+  const passwordInput = document.querySelector('input[name="password"]');
+  const confirmpasswordInput = document.querySelector('input[name="conf_password"]');
+  const firstnameInput = document.querySelector('input[name="firstname"]');
+  const lastnameInput = document.querySelector('input[name="lastname"]');
+  const emailInput = document.querySelector('input[name="email"]');
+  // send data to sendData
+  sendData(usernameInput.value, passwordInput.value, confirmpasswordInput.value, firstnameInput.value, lastnameInput.value, emailInput.value);
+});
+
+
+module.exports = {
+    sendHttpRequest,
+    sendData,
+    sendDataForTesting
+};
