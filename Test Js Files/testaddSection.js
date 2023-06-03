e28fafee907cdc7e2de05f16ed793e47f6476dd9
ilyas <ilyascoskun200@gmail.com>
2023-06-04 02:38:55 +0300

    added addsection files
    created actual Js file, test Js file and html file for the addsection page.

---
+++ Test Js Files/testaddSection.js
@@ -0,0 +1,110 @@
+window.addEventListener('load', function () {
+    const currDate = document.getElementById("currDate");
+    const date = new Date();
+    currDate.innerHTML = date.toLocaleDateString();
+
+  });
+
+const submitBtn = document.getElementById('submitSection-btn');
+
+const { username, sessionId } = getQueryParams();
+
+// get login parameters from login.js and use it here to get the courses of a user
+function getQueryParams() {
+  const search = window.location.search.substring(1);
+  const queryParams = new URLSearchParams(search);
+
+  return {
+    username: queryParams.get('username'),
+    sessionId: queryParams.get('sessionId')
+  };
+}
+
+const sendHttpRequest = (method, url, data) => {
+    return fetch(url, {
+      method: method,
+      body: JSON.stringify(data),
+      headers: data ? { 'Content-Type': 'application/json' } : {}
+    }).then(response => {
+      if (response.status >= 500) {
+        // !response.ok
+        return response.json().then(errResData => {
+          const error = new Error('Something went wrong!');
+          error.data = errResData;
+          throw error;
+        });
+      }
+      return response.json();
+    });
+};
+
+const sendData = (coursecode, sectionYear, sectionNumber, semester, teacherUserName) => {
+    sendHttpRequest('POST', 'http://44.203.249.113:8080/course/section/add', {
+        courseID: coursecode,
+        sectionYear: sectionYear,
+        sectionNumber: sectionNumber,
+        semester: semester,
+        teacherUserName: teacherUserName, 
+        adminUserName: username,
+        sessionId: sessionId
+    })
+      .then(responseData => {
+        console.log(responseData);
+      })
+      .catch(err => {
+        console.log(err, err.data);
+      });
+};
+
+const sendDataForTesting = (coursecode, sectionYear, sectionNumber, semester, teacherUserName) => {
+    sendHttpRequest('POST', 'http://44.203.249.113:8080/course/section/add', {
+        courseID: coursecode,
+        sectionYear: sectionYear,
+        sectionNumber: sectionNumber,
+        semester: semester,
+        teacherUserName: teacherUserName, 
+        adminUserName: username,
+        sessionId: sessionId
+    })
+      .then(responseData => {
+        if (responseData.success) {
+            return responseData.success;
+        } else {
+            return false;
+        }
+      })
+      .catch(err => {
+        console.log(err, err.data);
+      });
+};
+
+// get login parameters from login.js and use it here to get the courses of a user
+function getQueryParams() {
+  const search = window.location.search.substring(1);
+  const queryParams = new URLSearchParams(search);
+
+  return {
+    username: queryParams.get('username'),
+    sessionId: queryParams.get('sessionId')
+  };
+}
+
+// get data from html page
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+    const courseCodeInput = document.querySelector('input[name="courseCode"]');
+    const sectionYearInput = document.querySelector('input[name="sectionYear"]');
+    const sectionNumberInput = document.querySelector('input[name="sectionNumber"]');
+    const semesterInput = document.querySelector('input[name="semester"]');
+    const teacherUserNameInput = document.querySelector('input[name="teacherUserName"]');
+
+    // send data to sendData
+    sendData(courseCodeInput.value, sectionYearInput.value, sectionNumberInput.value, semesterInput.value, teacherUserNameInput.value);
+});
+
+module.exports = {
+    sendHttpRequest,
+    sendData,
+    sendDataForTesting
+};
